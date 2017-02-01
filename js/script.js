fa3371b7b8f93c40ec3424b6c368b121c3207be3
Anthony Zheng <Anthony@fopen-dream.space>
2017-02-01 02:17:38 -0500

    init

---
+++ js/script.js
@@ -0,0 +1,21 @@
+$(function() {
+
+    var home = new Vue({
+        el: "#homeContent",
+        data: {
+            what: "What is Weebchan?",
+            whatContent: "Weebchan is a simple image-based bulletin\
+                board where anyone can post comments and share images.\
+                There are boards dedicated to a variety of topics,\
+                from Japanese animation and culture to videogames,\
+                music, and photography. Users do not need to register\
+                an account before participating in the community. Feel\
+                free to click on a board below that interests you and jump right in!",
+            whatRule: "Be sure to familiarize yourself with the Rules\
+                before posting, and read the FAQ if you wish to learn more\
+                about how to use the site."
+        }
+
+    });
+
+});
